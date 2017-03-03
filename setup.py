1022dbe64833abf00436f97bc6cf663b68e790e6
Gonzalo Fernandez <gonzalo.fernandez@imagination.com>
2017-03-03 17:25:39 +0000

    Bump version: 0.0.5 → 0.0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='patilloid',
-    version='0.0.5',
+    version='0.0.6',
     description='',
     license='GPLv2',
     packages=find_packages(),
